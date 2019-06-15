6fe24dff6f27cd83966949e3184733395fc99815
Javier Gutierrez <javier.guse@icloud.com>
2019-06-15 18:52:53 -0400

    refactor recipe item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,9 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() name: string;
-  @Input() description: string;
-  @Input() imagePath: string;
+  @Input() recipe: Recipe
 
   constructor() { }
 
