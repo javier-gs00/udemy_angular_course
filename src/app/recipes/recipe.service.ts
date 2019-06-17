af98c5e0b2506c55ccf7d7494cd00bc56a1cedef
Javier Gutierrez <javier.gutierrez@2brains.cl>
2019-06-17 18:17:05 -0400

    add | recipe and shopping list services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>()
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      ),
+    new Recipe(
+      'Olive Oil',
+      'Oil',
+      'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+      ),
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
