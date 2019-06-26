dcbe8dcf7e8ec2ea6062532eed3ec1b2e8264a65
Javier Gutierrez <javier.guse@icloud.com>
2019-06-25 22:06:46 -0400

    add | http module with firabase backend

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,28 +7,34 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Steak and salad',
-      'The usual steak with salad',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('meat', 1),
-        new Ingredient('salads', 2),
-      ]
-      ),
-    new Recipe(
-      'Olive Oil',
-      'Oil',
-      'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
-      [
-        new Ingredient('oil', 1),
-        new Ingredient('olives', 5),
-      ]
-      ),
-  ];
+  private recipes: Recipe[] = []
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Steak and salad',
+  //     'The usual steak with salad',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [
+  //       new Ingredient('meat', 1),
+  //       new Ingredient('salads', 2),
+  //     ]
+  //     ),
+  //   new Recipe(
+  //     'Olive Oil',
+  //     'Oil',
+  //     'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+  //     [
+  //       new Ingredient('oil', 1),
+  //       new Ingredient('olives', 5),
+  //     ]
+  //     ),
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes
+    this.recipesChanged.next(this.recipes.slice())
+  }
 
   getRecipes() {
     return this.recipes.slice();
