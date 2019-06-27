0c1a698fa784611afa13eaa6416ef12acac9cfa3
Javier Gutierrez <javier.guse@icloud.com>
2019-06-27 13:40:08 -0400

    enhance | header component to reflect auth state

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { OnInit, Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -12,7 +13,7 @@ export class AuthComponent implements OnInit {
     isLoading = false
     error: string | null = null
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     ngOnInit() {}
 
@@ -37,6 +38,7 @@ export class AuthComponent implements OnInit {
         authObs.subscribe(resData => {
             console.log(resData)
             this.isLoading = false
+            this.router.navigate(['/recipes'])
         }, errorMessage => {
             console.log(errorMessage)
             this.error = errorMessage
