190554790102edf363b146de90bc1a0a4261b2b4
Javier Gutierrez <javier.guse@icloud.com>
2019-06-28 22:12:21 -0400

    add | autologin and autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -20,6 +20,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User>(null)
+    private tokenExpirationTimer: any
 
     constructor(private http: HttpClient, private router: Router) {}
 
@@ -58,6 +59,42 @@ export class AuthService {
     logout() {
         this.user.next(null)
         this.router.navigate(['/auth'])
+        localStorage.removeItem('userData')
+        if (this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer)
+        }
+        this.tokenExpirationTimer = null
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout()
+        }, expirationDuration)
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'))
+        if (!userData) {
+            return
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        )
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser)
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+            this.autoLogout(expirationDuration)
+        }
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
@@ -69,6 +106,8 @@ export class AuthService {
             expirationDate
         )
         this.user.next(user)
+        this.autoLogout(expiresIn * 1000)
+        localStorage.setItem('userData', JSON.stringify(user))
     }
 
     private handleError(errorRes: HttpErrorResponse) {
